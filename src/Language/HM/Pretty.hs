95c787f3a23c48082c446fbc09db4b1832d333be
Dr. ERDI Gergo <gergo@erdi.hu>
2016-05-04 19:28:20 +0800

    Modularize pPrint for Types

--- src/Language/HM/Pretty.hs
+++ src/Language/HM/Pretty.hs
@@ -8,11 +8,8 @@ import Language.HM.Remap
 import Control.Unification
 import Data.Void
 import Control.Monad.Reader
-import Control.Monad.Writer hiding (Product)
 import Data.Functor.Product
 import Data.Functor.Constant
-import Data.Set (Set)
-import qualified Data.Set as Set
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Data.Maybe
@@ -22,28 +19,39 @@ import Data.Stream (Stream(..))
 import qualified Data.Stream as Stream
 import Data.List (nub)
 
+type Ppr v = Staged [v] (v -> Doc)
+
+pprVar :: v -> Ppr v Doc
+pprVar x = Pair (Constant [x]) (asks ($ x))
+
+pprType :: Rational -> UTerm Ty0 v -> Ppr v Doc
+pprType = go
+  where
+    go p t0@(UTerm t) = case t of
+        TApp{} -> case tApps t0 of
+            (UTerm (TCon "Fun"), [t, u]) ->
+                (\ t u -> par 1 $ t <+> text "->" <+> u) <$> go 1 t <*> go 0 u
+            (tcon, []) -> go 0 tcon
+            (tcon, targs) -> (\t ts -> par 2 $ t <+> hsep ts) <$> go 0 tcon <*> traverse (go 2) targs
+        TCon tcon -> pure $ text tcon
+      where
+        par i | p >= i = parens
+              | otherwise = id
+    go p (UVar x) = pprVar x
+
+runPpr :: (Ord v) => Ppr v a -> a
+runPpr (Pair (Constant tvars) fill) =
+    runReader fill $ \v -> fromJust $ Map.lookup v mapping
+  where
+    mapping = Map.fromList $ zip (nub tvars) (Stream.toList niceTVars)
+
+niceTVars :: Stream Doc
+niceTVars = fmap text $
+            Stream.prefix ["α", "β", "c", "d", "e", "f"] $
+            fmap (\i -> 'a' : show i) $ Stream.iterate succ 0
+
 instance Pretty Ty where
     pPrintPrec level prec t = pPrintPrec level prec (unfreeze t :: UTerm Ty0 Void)
 
-niceTVars :: Stream String
-niceTVars = Stream.prefix ["α", "β", "c", "d", "e", "f"] $
-            fmap (\i -> 'a' : show i) $ Stream.iterate succ 0
-
 instance (Eq v, Ord v) => Pretty (UTerm Ty0 v) where
-    pPrintPrec level prec t = case go prec t of
-        Pair (Constant tvars) fill ->
-            runReader fill $ Map.fromList $ zip (nub tvars) (Stream.toList niceTVars)
-      where
-        go p t0@(UTerm t) = case t of
-            TApp{} -> case tApps t0 of
-                (UTerm (TCon "Fun"), [t, u]) ->
-                    (\ t u -> par 1 $ t <+> text "->" <+> u) <$> go 1 t <*> go 0 u
-                (tcon, []) -> go 0 tcon
-                (tcon, targs) -> (\t ts -> par 2 $ t <+> hsep ts) <$> go 0 tcon <*> traverse (go 2) targs
-            TCon tcon -> pure $ text tcon
-          where
-            par i | p >= i = parens
-                  | otherwise = id
-        go p (UVar a) = text <$> remap a
-
-        remap x = Pair (Constant [x]) (asks $ fromJust . Map.lookup x)
+    pPrintPrec level prec t = runPpr $ pprType prec t
